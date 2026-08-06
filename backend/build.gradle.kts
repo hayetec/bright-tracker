b8d07e0b3c2b6b9a5031f0a505aefb0fa6a81bda
Isse Nur <issenur@gmail.com>
2026-08-06 14:37:06 -0500

    feat: configure PostgreSQL persistence and Flyway

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -19,8 +19,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.flywaydb:flyway-core")
+
+	implementation("org.flywaydb:flyway-database-postgresql")
+
+	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
