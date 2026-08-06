b6d41e89632c6c3ea611944cf3ba2e866349f599
Isse Nur <issenur@gmail.com>
2026-08-06 13:44:31 -0500

    initialized Spring Boot backend

---
+++ backend/src/main/kotlin/com/issenur/brighttracker/BrightTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.issenur.brighttracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrightTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrightTrackerApplication>(*args)
+}
